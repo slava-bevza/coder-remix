1a39ec5f7c07566ea8ff444202b361f282aeaa2e
UDEX <137228334+udex-app@users.noreply.github.com>
2024-07-16 23:55:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,8 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  server: {
+    host: '0.0.0.0', // Change this to a valid IP address if needed
+    port: 5173, // Optional otherwise your app will start on default port
+  },
 });
